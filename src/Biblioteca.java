22f5001c2163872d5c8dc059dd61f7730d59d014
Alejandro Muriel <amuripra@myuax.com>
2026-03-02 11:58:26 +0100

    feat (Libro): Creada la clase Libro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,34 @@
 public class Biblioteca {
+
+    //Atributos
+    private Libro[] catalogo;
+    private final int CANT_MAX_LIBROS;
+
+    /** CONSTRUCTOR:
+     * @param
+     */
+    public Biblioteca(int cantidad_maxima_libro) {
+        this.CANT_MAX_LIBROS = cantidad_maxima_libro;
+
+        //Parametros
+        catalogo = new Libro[CANT_MAX_LIBROS];
+
+    }
+
+    private Libro buscarSala(int codigoLibro) {
+        boolean libroEncontrado = false;
+        Libro Libro = null;
+        int i = 0;
+
+        while (i < CANT_MAX_LIBROS && !libroEncontrado) {
+            if (catalogo[i] != null && catalogo[i].getId() == codigoLibro) {
+                libroEncontrado = true;
+                Libro = catalogo[i];
+            }
+            i++;
+        }
+        return Libro;
+    }
+
+    //Gets
 }
--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,74 @@
 public class Libro {
+
+    //Atributos
+    private int id;
+    private String titulo;
+    private String autor;
+    private int numPaginas;
+
+
+    /** CONSTRUCTOR:
+     * @param id
+     * @param titulo
+     * @param autor
+     * @param numPaginas
+     */
+    public Libro(int id, String titulo, String autor, int numPaginas) {
+        int numHojas; //Variable del metodo
+
+        //Parametros
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+
+        numHojas = numPaginas/2;
+    }
+
+    //Gets
+
+    /**
+     *
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    //
+    @Override //Override porque las clases cojen los metodos del padre clase, por eso hay que sobreescribir su funcionamiento
+    public String toString() {
+        StringBuilder infoLibro = new StringBuilder ("Libro: ");
+
+        infoLibro.append (String.format("\n id: %d", id));
+        infoLibro.append (String.format("\n titulo: %s\n", titulo));
+        infoLibro.append (String.format("\n autor: %s\n", autor));
+        infoLibro.append (String.format("\n numPaginas: %d\n", numPaginas));
+
+        return infoLibro.toString();
+    }
 }
